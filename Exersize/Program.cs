973897a5f22bfdad650fa594f70f52172937b9c4
Realrett <gosha-student@mail.ru>
2022-12-04 15:01:17 +0700

    Задаём массив

---
+++ Exersize/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив строк, 
+длина которых меньше либо равна 3 символам.*/
+
+int[] InitArray()
+	{
+	    int[] res = new int[10];
+	    Random rnd = new Random();
+	    for (int i = 0; i < res.Length; i++)
+	    {
+	        res[i] = rnd.Next(0,100);
+	    }
+	    return res;
+	}
