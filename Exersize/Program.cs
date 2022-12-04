6f0313e94169f1df63b1e3e508cbfd5b73fa6eba
Realrett <gosha-student@mail.ru>
2022-12-04 15:03:00 +0700

    Задаём массив

--- Exersize/Program.cs
+++ Exersize/Program.cs
@@ -3,11 +3,20 @@
 
 int[] InitArray()
 	{
-	    int[] res = new int[10];
+	    int[] res = new int[n];
 	    Random rnd = new Random();
 	    for (int i = 0; i < res.Length; i++)
 	    {
-	        res[i] = rnd.Next(0,100);
+	        res[i] = rnd.Next(-10000,10001);
 	    }
 	    return res;
+	}
+
+    void PrintArray(int[] array)
+	{
+	    for (int i = 0; i < array.Length; i++)
+	    {
+	        Console.Write($"{array[i]} ");
+	    }
+	    Console.WriteLine();
 	}
